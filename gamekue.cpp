2bb6599878da870e2ec133b3e28d99c7cf092e28
ramapraditharyananda <ramapro2006@gmail.com>
2024-10-07 15:16:37 +0700

    Update gamekue.cpp

--- gamekue.cpp
+++ gamekue.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
-    srand(time(nullptr));
-    
-    int pilih;
-    
-    
-    while(true){
-        system("clsc");
-        int cake = 1 + rand() % 9;
-        
-        cout<<"==================="<<endl;
-        cout<<"|  1  |  2  |  3  |"<<endl;
-        cout<<"|  4  |  5  |  6  |"<<endl;
-        cout<<"|  7  |  8  |  9  |"<<endl;
-        cout<<"==================="<<endl;
-        
-        // cout<<cake <<endl; Jika mau lihat
-        cout<<"Silahkan pilih salah satu angka diatas untuk mencari kue: ";
-        cin>>pilih;
-        
-        if(pilih == cake){
-            cout<<"Yey, Anda mendapatkan kue!"<<endl;
-        }else{
-            cout<<"Salah, kue berada di angka nomor "<< cake <<endl;
-        }
-        
-        cin.get();
-        cout<<"tekan enter untuk bermain lagi..";
-        cin.get();
+void pilih();
+
+void pilih(){
+    int pilihan; 
+    cout << "SELAMAT DATANG DI SWEET GUESS" << endl;
+    cout << " ⋆｡‧˚ʚ🍒 ɞ˚ ˚🍰｡˚ ᡣ𐭩 ⋆" << endl;
+    cout << "Apakah Anda siap bermain game?" << endl;
+    cout << "Ketik 1 untuk Ya" << endl;
+    cout << "Ketik 2 untuk Tidak" << endl;
+    cout << "Masukkan pilihan anda : ";
+    cin >> pilihan;
+    if (pilihan == 2) {
+        cout << "Game Sweet Guess telah terlupakan well 🤟" << endl;
+        return; // Exit the function if the user chooses 'Tidak'
+    } else if (pilihan == 1) {
+        int pilihanAngka;
+    } else {
+        cout<<"pilihan nya 1 dan 2 well 🤟"<<endl;
+        return; // Exit the function if the user chooses 'Tidak'
     }
     
+    int pilihanAngka;
+    
+    // Menggunakan waktu saat ini sebagai seed untuk angka acak
+    
+
+    while(true){
+        int kue = rand() % 9;  // Menghasilkan huruf acak antara 1 dan 9
+
+        cout << "===================" << endl;
+        cout << "|  1  |  2  |  3  |" << endl;
+        cout << "|  4  |  5  |  6  |" << endl;
+        cout << "|  7  |  8  |  9  |" << endl;
+        cout << "===================" << endl;
+
+        cout << "Silahkan pilih salah satu angka di atas untuk mencari sweet cake: ";
+        cin >> pilihanAngka;
+        
+        if(pilihanAngka == kue){
+            cout << "Yey, Anda mendapatkan SWEET CAKE!🤟" << endl;
+        } else {
+            cout << "Salah, sweet cake berada di angka nomor " << kue << endl;
+        }
+        
+        if(pilihan!=1){
+            cout << "Terima Kasih telah bermain game kami" << endl;
+        }
+    
+
+        cout << "Tekan enter untuk bermain lagi.." << endl;
+        cin.ignore();  // Mengabaikan input tersisa
+        cin.get();     // Menunggu tombol Enter ditekan
+    }
+}
+
+int main(){
+    pilih();
     return 0;
 }
